88bb27cafe9394955cba107b954dc5e1b664e015
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-20 03:20:24 +0300

    add new deps

--- resources/js/components/containers/LoginContainer.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import "./Login.css";
-
-export default class LoginContainer extends Component {
-    
-}
