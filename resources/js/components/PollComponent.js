9b47b154391ac3000a8c2cf6d5636cc9b75a2cfc
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-22 18:02:00 +0300

    add skeleton classes for PollComponent and NewPollAdderComponent

---
+++ resources/js/components/PollComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class PollComponent extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
--- resources/js/components/PollFeedComponent.js
+++ resources/js/components/PollFeedComponent.js
@@ -1,9 +1,59 @@
 import React, {Component} from 'react';
+import Waypoint from 'react-waypoint';
+import PollComponent from './PollComponent';
+import NewPollAdderComponent from './NewPollAdderComponent';
+
+
 
 export default class PollFeedComponent extends Component {
+
+    _loadMoreItems() {
+        this.setState({isLoading: true});
+        // TODO loading
+    }
+
+    _renderNewPollAdder() {
+        return (
+            <NewPollAdderComponent/>
+        )
+    }
+
+    _renderItems() {
+        return this.state.items(function (poll) {
+            return (
+                <PollComponent
+                    key={poll.id}
+                    title={poll.title}
+                    options={poll.options}
+                    isAnonymous={poll.isAnonymous}
+                    isMultianswer={poll.isMultianswer}
+                    userVotedFor={poll.userVotedFor}
+                />
+            );
+        });
+    }
+
+    _renderWaypoint() {
+        if (this.state.isLoading) {
+            // TODO return 'Loading' component
+        }
+        else {
+            return (
+                <Waypoint
+                    onEnter={this._loadMoreItems}
+                    threshold={2.0}
+                />
+            );
+        }
+    }
+
     render() {
         return (
-            <div></div>
+            <div>
+                {this._renderNewPollAdder()}
+                {this._renderItems()}
+                {this._renderWaypoint()}
+            </div>
         )
     }
 }
