77dc409415b00a94dd693892de387b871c4a7887
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-23 23:59:29 +0300

    update routes so vkcallback would connect to web middleware to persist session to pass jwt token back to client after oauth

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
+Route::get('vkcallback', 'VKOAuthCallbackController@index');
+
 Route::get( '/{path?}', function(){
-    return view( 'welcome' );
-} )->where('path', '.*');
+    error_log('token: ' . session()->get('jwtToken'));
+    return view('welcome');
+} )->where('path', '.*')->name('main');
