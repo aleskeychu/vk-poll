b7637ac80f26a86371325d210f0bbb4cc0c67712
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-25 20:54:33 +0300

    [components] connect LoginComponent to store.isAuthenticated

--- resources/js/components/LoginComponent.js
+++ resources/js/components/LoginComponent.js
@@ -1,20 +1,34 @@
 import React, {Component} from 'react';
 import {Button} from 'reactstrap';
+import {Redirect} from 'react-router';
+import {connect} from 'react-redux';
 
 //TODO change client_id to real id
 const vk_url = 'https://oauth.vk.com/authorize?client_id=6727304&redirect_uri=http://localhost:8000/vkcallback';
 
 const redirectToVK = () => {
-  window.location.href = vk_url;
+    window.location.href = vk_url;
 };
 
-
-export default class LoginComponent extends Component {
+class LoginComponent extends Component {
     render() {
-        return (
-            <div>
-                <Button onClick={redirectToVK}/>
-            </div>
-        )
+        return this.props.isAuthenticated
+            ? (
+                <Redirect to='/feed'/>
+
+            )
+            : (
+                <div>
+                    <Button onClick={redirectToVK}/>
+                </div>
+            );
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.isAuthenticated
+    };
+};
+
+export default connect(mapStateToProps)(LoginComponent);
