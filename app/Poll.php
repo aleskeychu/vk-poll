204ce36cc84e614f608a4dcb8008b5cfb109a10f
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-26 15:24:59 +0300

    dont return updated_at for poll from backend

--- app/Poll.php
+++ app/Poll.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Poll extends Model
 {
     protected $fillable = ['title', 'is_multianswer', 'is_anonymous', 'user_id'];
+
+    protected $hidden = ['updated_at'];
 }
