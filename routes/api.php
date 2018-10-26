38321d47764e9e88bce54d031218c4ddad98e4e6
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-26 15:22:49 +0300

    [backend] add route for feed"

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group(['middleware' => 'jwt.auth'], function () {
     Route::resource('poll', 'PollController');
+    Route::get('polls', 'FeedController@index');
 });
