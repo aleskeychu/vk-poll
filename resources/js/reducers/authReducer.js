499fd0436cf32acdbfe9dbf7ad6fe02145e47572
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-27 01:23:55 +0300

    properly reduce error actions

--- resources/js/reducers/authReducer.js
+++ resources/js/reducers/authReducer.js
@@ -1,12 +1,16 @@
-import { LOGOUT } from '../constants/actions_types';
+import {DELETE_POLL_ERROR, ERROR_CREATING_POLL, LOGOUT} from '../constants/actions_types';
 
 export default function authReducer(state = {}, action) {
     switch (action.type) {
+        case DELETE_POLL_ERROR:
+        case ERROR_CREATING_POLL:
+        case EDIT_POLL_ERROR:
+            if (action.error === USER_NOT_FOUND) {
+                return '';
+            }
+            return state;
         case LOGOUT:
-            return {
-                ...state,
-                isAuthenticated: ''
-            };
+            return '';
         default:
             return state;
     }
