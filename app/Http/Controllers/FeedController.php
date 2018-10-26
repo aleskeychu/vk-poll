9dabbc7ff23200316e780229a4c42f32e8ddf039
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-26 17:00:57 +0300

    update feed controller to also return options and votes

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -15,13 +15,12 @@ class FeedController extends Controller
         ]);
         $amount = $data['amount'] ?? 20;
         if (!isset($data['poll_id'])) {
-            $polls = Poll::orderBy('created_at', 'desc')->take($amount)->with('options')->get();
+            $polls = Poll::orderBy('created_at', 'desc')->take($amount)->with('user')->with('options')->with('votes')->get();
         } else {
             $mark = Poll::find($data['poll_id']);
             $polls = Poll::where('created_at', '<', $mark->created_at)->order_by('created_at', 'desc')
                 ->take($amount)->with('options')->get();
         }
-
         return response()->json($polls);
     }
 }
