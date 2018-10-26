6205c18bd8a9c64bcc4ae2319f150d38d12e84f5
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-27 01:27:22 +0300

    create skeleton component for displaying user info

---
+++ resources/js/components/UserComponent.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+export default class UserComponent extends Component {
+    render() {
+        return (
+          <div>
+
+          </div>
+        );
+    }
+}
