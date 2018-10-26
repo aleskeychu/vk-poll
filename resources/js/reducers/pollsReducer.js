95de5188eae10797da2f13c8a3470615f2050a89
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-27 01:24:29 +0300

    change polls reducer to work properly after using combineReducers

--- resources/js/reducers/pollsReducer.js
+++ resources/js/reducers/pollsReducer.js
@@ -4,33 +4,19 @@ import update from 'immutability-helper';
 export default function pollsReducer(state = {}, action) {
     switch (action.type) {
         case REFRESH_POLLS:
-            return {
-                ...state,
-                polls: action.polls
-            };
+            return action.polls;
         case FETCH_MORE_POLLS:
             // leave only those polls which are not yet in the state
             const newPolls = action.polls.filter(poll => {
-                return state.polls.find(other => other.id === poll.id) !== undefined
+                return state.find(other => other.id === poll.id) === undefined
             });
-            return {
-                ...state,
-                polls: state.polls.concat(newPolls)
-            };
+            return state.concat(newPolls);
         case EDIT_POLL_SUCCESS:
             const idxUpdate = state.polls.findIndex(elem => elem.id === action.poll.id);
-            const updatedPolls = update(state.polls, {[idxUpdate]: {$set: action.poll}});
-            return {
-                ...state,
-                polls: updatedPolls
-            };
+            return update(state.polls, {[idxUpdate]: {$set: action.poll}});
         case DELETE_POLL:
             const idxDelete = state.polls.findIndex(elem => elem.id === action.poll.id);
-            const pollsAfterDeletion = update(state.poll, {$unset: [idxDelete]});
-            return {
-                ...state,
-                polls: pollsAfterDeletion
-            };
+            return update(state.poll, {$unset: [idxDelete]});
         default:
             return state;
     }
