676b2007c4f47784eda13ebb20a13e3089205e34
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-27 19:22:53 +0300

    add action constants for voting

--- resources/js/constants/actions_types.js
+++ resources/js/constants/actions_types.js
@@ -2,7 +2,8 @@ export const TEST = 'TEST';
 
 export const REFRESH_POLLS = 'REFRESH_POLLS';
 export const FETCH_MORE_POLLS = 'FETCH_MORE_POLLS';
-export const DELETE_POLL = 'DELETE_POLL';
+
+export const ERROR_FETCHONG_POLLS = 'ERROR_FETCHING_POLLS';
 
 export const LOGOUT = 'LOGOUT';
 
@@ -24,3 +25,6 @@ export const EDIT_POLL_ERROR = 'EDIT_POLL_ERROR';
 export const DELETE_POLL_STARTED = 'DELETE_POLL_STARTED';
 export const DELETE_POLL_SUCCESS = 'DELETE_POLL_SUCCESS';
 export const DELETE_POLL_ERROR = 'DELETE_POLL_ERROR';
+
+export const VOTED = 'VOTED';
+export const VOTED_ERROR = 'voted_error';
