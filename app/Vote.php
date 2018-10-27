f9f327d938eeb42b145dc8d7452db4f7e236005d
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-27 19:17:49 +0300

    add fillable and hidden fields to Vote model

--- app/Vote.php
+++ app/Vote.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vote extends Model
 {
+    protected $fillable = [
+        'poll_id', 'vote_id', 'user_id'
+    ];
+
+    protected $hidden = [
+        'created_at', 'updated_at'
+    ];
+
     public function user() {
         return $this->hasOne('App\User');
     }
