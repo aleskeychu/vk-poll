a74d7c44ecbc6aaee9c20a3e6d564f3db612cf4c
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-27 19:16:50 +0300

    in FeedController add fields to check what for the user has voted, dont return votes array if poll is anonymous

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use \App\Poll;
+use Auth;
 
 class FeedController extends Controller
 {
@@ -21,6 +22,32 @@ class FeedController extends Controller
             $polls = Poll::where('created_at', '<', $mark->created_at)->order_by('created_at', 'desc')
                 ->take($amount)->with('options')->get();
         }
+        $polls = $this->addFieldUserVoted($polls->toArray());
+        $polls = $this->filterForAnonymous($polls);
         return response()->json($polls);
+    }
+
+    private function addFieldUserVoted($polls) {
+        $userId = Auth::user()->id;
+        return array_map(function($poll) use ($userId) {
+            $vote = array_filter($poll['votes'], function($vote) use ($userId) {
+                return $vote['user_id'] === $userId;
+            });
+            if (!empty($vote)) {
+                $poll['userVotedFor'] = $vote[0]['vote_id'];
+            } else {
+                $poll['userVotedFor'] = -1;
+            }
+            return $poll;
+        }, $polls);
+    }
+
+    private function filterForAnonymous($polls) {
+        return array_map(function($poll) {
+            if ($poll['is_anonymous']) {
+                $poll['votes'] = [];
+            }
+            return $poll;
+        }, $polls);
     }
 }
