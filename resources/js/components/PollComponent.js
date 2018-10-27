5dfc943cdb8082419411f8f86645f9993e33e9da
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-27 19:28:42 +0300

    In PollComponent add onClick callback for voting, wrap voted option into a separate class, properly handle props from container component

--- resources/js/components/PollComponent.js
+++ resources/js/components/PollComponent.js
@@ -1,31 +1,40 @@
 import React, {Component} from 'react';
 import {Button, Label, Row} from "react-bootstrap";
+import {pollType} from '../types';
+import PropTypes from 'prop-types';
 
 export default class PollComponent extends Component {
 
     render() {
-        const userHasVoted = this.props.userVotedFor !== -1;
+        const userHasVoted = this.props.poll.userVotedFor !== -1;
+        const creatorIsCurrentUser = this.props.userId === this.props.poll.user_id;
+
         // TODO change edit and delete to dropdown
         return (
             <div><Row>
                 {/*<UserComponent user={this.props.user} />*/}
-                {this.props.creatorIsCurrentUser
+                {creatorIsCurrentUser
                     ? (<div>
                         <Button onClick={this.props.onEdit}>edit</Button>
                         <Button onClick={this.props.onDelete}>delete</Button>
                     </div>)
                     : null
                 }
-                </Row>
+            </Row>
 
-                <Row><h3><Label>{this.props.title}</Label></h3></Row>
+                <Row><h3><Label>{this.props.poll.title}</Label></h3></Row>
                 {
-                    this.props.options.map((elem, idx) => {
+                    this.props.poll.options.map((elem, idx) => {
                         let poll = userHasVoted
-                            ? (<Row key={idx}><h4 ><Label>Label: {elem.text + ' (' + elem.count + ')'} </Label></h4></Row>)
-                            : (<Row key={idx}><h4 ><Button>Button: {elem.text + ' (' + elem.count + ')'}</Button></h4></Row>);
-                        if (userHasVoted && idx === this.props.userVotedFor) {
-                            poll = (<div className='votedByUser'>{poll}</div>);
+                            ? (<Row key={idx}><h4><Label>{elem.text + ' (' + elem.vote_count + ')'} </Label></h4></Row>)
+                            : (<Row key={idx}><h4>
+                                <Button
+                                    onClick={this.props.onVote(this.props.poll.id, elem.index, this.props.userId)}>
+                                    {elem.text + ' (' + elem.vote_count + ')'}
+                                </Button>
+                            </h4></Row>);
+                        if (userHasVoted && elem.index === this.props.poll.userVotedFor) {
+                            poll = (<div key={idx} className='votedByUser'>{poll}</div>);
                         }
                         return poll;
                     })
@@ -34,3 +43,11 @@ export default class PollComponent extends Component {
         );
     }
 }
+
+PollComponent.propTypes = {
+    poll: PropTypes.shape(pollType),
+    userId: PropTypes.number,
+    onEdit: PropTypes.func,
+    onDelete: PropTypes.func,
+    onVote: PropTypes.func
+};
