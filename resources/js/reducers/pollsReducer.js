656f996ec985342af344df3b6871c282cda2f719
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-28 16:37:33 +0300

    properly reduce actions edit_poll_success and unvoted, refactor voted reducer to not alter votes (as they are no longer passed in from controller)

--- resources/js/reducers/pollsReducer.js
+++ resources/js/reducers/pollsReducer.js
@@ -1,30 +1,51 @@
-import {FETCH_MORE_POLLS, REFRESH_POLLS, EDIT_POLL_SUCCESS, VOTED, DELETE_POLL_SUCCESS} from "../constants/actions_types";
+import {
+    FETCH_MORE_POLLS,
+    REFRESH_POLLS,
+    EDIT_POLL_SUCCESS,
+    VOTED,
+    DELETE_POLL_SUCCESS,
+    UNVOTED
+} from "../constants/actions_types";
 import update from 'immutability-helper';
 
 export default function pollsReducer(state = {}, action) {
     switch (action.type) {
-        case VOTED:
+        case VOTED: {
             let newState = state;
             const idxAlter = state.findIndex(poll => poll.id === action.poll_id);
-            // adding new vote to 'voted' array of the poll
-            if (!state[idxAlter].is_anonymous) {
-                newState = update(state, {
-                    [idxAlter]: {
-                        votes:
-                            {$push: [{user_id: action.user_id, vote_id: action.option_id}]}
-                    }
-                });
-            }
-            // increasing counter of votes for the voted option
-            const votedOptionIdx = newState[idxAlter].options.findIndex(option => option.index === action.option_id);
-            const newCount = newState[idxAlter].options[votedOptionIdx].vote_count + 1;
+            // const votes = action.option_ids.map(function (vote_id) {
+            //     return {
+            //         user_id: action.user_id,
+            //         vote_id
+            //     }
+            // });
+            // // adding new vote to 'voted' array of the poll
+            // if (!state[idxAlter].is_anonymous) {
+            //     newState = update(state, {
+            //         [idxAlter]: {
+            //             votes:
+            //                 {$push: votes}
+            //         }
+            //     });
+            // }
+
+            // increasing counter of votes for the voted options
+            const votedOptionIndices = action.option_ids.map(function (option_id) {
+                return newState[idxAlter].options.findIndex(option => option.index === option_id)
+            });
             newState = update(newState, {
                 [idxAlter]: {
                     options: {
-                        [votedOptionIdx]: {
-                            vote_count: {
-                                $set: newCount
-                            }
+                        $apply: (options) => {
+                            return options.map((option, idx) => {
+                                if (votedOptionIndices.find(index => index === idx) !== undefined) {
+                                    return {
+                                        ...option,
+                                        vote_count: option.vote_count + 1
+                                    };
+                                }
+                                return option;
+                            });
                         }
                     }
                 }
@@ -33,23 +54,60 @@ export default function pollsReducer(state = {}, action) {
             return update(newState, {
                 [idxAlter]: {
                     userVotedFor: {
-                        $set: action.option_id
+                        $set: action.option_ids
                     }
                 }
             });
-        case REFRESH_POLLS:
+        }
+        case UNVOTED: {
+            const idxDelete = state.findIndex(poll => poll.id = action.poll_id);
+            const userVotedFor = state[idxDelete].userVotedFor;
+            return update(state, {
+               [idxDelete]: {
+                   userVotedFor: {
+                       $set: []
+                   },
+                   options: {
+                       $apply: (options) => {
+                           return options.map((option, idx) => {
+                               if (userVotedFor.find(index => index === option.index) !== undefined) {
+                                   return {
+                                       ...option,
+                                       vote_count: option.vote_count - 1
+                                   }
+                               }
+                               return option;
+                           });
+                       }
+                   }
+               }
+            });
+        }
+        case REFRESH_POLLS: {
             return action.polls;
-        case FETCH_MORE_POLLS:
+        }
+        case FETCH_MORE_POLLS: {
             const newPolls = action.polls.filter(poll => {
                 return state.find(other => other.id === poll.id) === undefined
             });
             return state.concat(newPolls);
-        case EDIT_POLL_SUCCESS:
-            const idxUpdate = state.findIndex(elem => elem.id === action.poll.id);
-            return update(state, {[idxUpdate]: {$set: action.poll}});
-        case DELETE_POLL_SUCCESS:
+        }
+        case EDIT_POLL_SUCCESS: {
+            const idxUpdate = state.findIndex(elem => elem.id === action.id);
+            return update(state,
+                {
+                    [idxUpdate]:
+                        {
+                            title: {$set: action.title},
+                            options: {$set: action.options}
+                        }
+                }
+            );
+        }
+        case DELETE_POLL_SUCCESS: {
             const idxDelete = state.findIndex(elem => elem.id === action.id);
             return update(state, {$splice: [[idxDelete, 1]]});
+        }
         default:
             return state;
     }
