a8c7085479f259e51ad3a8973f2c3cca7c87bf5c
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-29 02:29:43 +0300

    add user component

--- resources/js/components/UserComponent.js
+++ resources/js/components/UserComponent.js
@@ -1,12 +1,31 @@
 import React, {Component} from 'react';
-
+import PropTypes from 'prop-types';
+import {userType} from "../types";
 
 export default class UserComponent extends Component {
-    render() {
-        return (
-          <div>
+    vk_url = 'https://vk.com/';
 
-          </div>
+    render() {
+        // get rid of seconds
+        const t = this.props.creationTime;
+        const time = t.substring(0, t.lastIndexOf(':'));
+        return (
+            <div style={{display: 'flex', alignItems: 'center'}}>
+                <a href={this.vk_url + this.props.user.domain}><img style={{borderRadius: '50%'}} src={this.props.user.image_50}/></a>
+                <div style={{display: 'flex', flexDirection: 'column', margin: '0 5px'}}>
+                    <div style={{margin: '0 5px'}}>
+                        {this.props.user.first_name + ' ' + this.props.user.second_name}
+                    </div>
+                    <div>
+                        {time}
+                    </div>
+                </div>
+            </div>
         );
     }
 }
+
+UserComponent.propTypes = {
+    user: PropTypes.shape(userType).isRequired,
+    creationTime: PropTypes.string.isRequired
+};
