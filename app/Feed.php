001b766feda98689151501231f211edca7f3ef32
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-30 19:27:35 +0300

    add method to invalidate feed in cache

--- app/Feed.php
+++ app/Feed.php
@@ -8,8 +8,17 @@
 
 namespace App;
 
+use Cache;
 
 class Feed
 {
+    const CACHE_TTL = 60 * 6;
 
+    public static function invalidateCacheForFeed() {
+        if (Cache::has('feed_version')) {
+            Cache::increment('feed_version');
+        } else {
+            Cache::put('feed_version', 1, self::CACHE_TTL);
+        }
+    }
 }
