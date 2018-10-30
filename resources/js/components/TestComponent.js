df4151e8969de8de9fd105866283e81cb4eb8365
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-30 20:14:18 +0300

    delete unneeded imports

--- resources/js/components/TestComponent.js
+++ resources/js/components/TestComponent.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import Header from './Header';
-import Footer from './Footer';
 import LoginComponent from './LoginComponent';
 import PrivateRoute from "./PrivateRoute";
 import MainComponent from './MainComponent';
