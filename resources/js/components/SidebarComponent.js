9cd2b45d289d5b36a71df56576392dd52dc35f40
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-30 20:13:56 +0300

    Add SidebarComponent to show currently logged in user

--- resources/js/components/SidebarComponent.js
+++ resources/js/components/SidebarComponent.js
@@ -1,9 +1,30 @@
 import React, {Component} from 'react';
+import stickySidebar from '../styles/stickySidebar.css';
+import {connect} from 'react-redux';
+import UserComponent from "./UserComponent";
+import {Button} from 'react-bootstrap';
 
-export default class SidebarComponent extends Component {
+class SidebarComponent extends Component {
+    logout = () => {
+        window.localStorage.setItem('jwtToken', '');
+        window.location.href = 'http://localhost:8000/login';
+    };
+
     render() {
         return (
-            <div></div>
+            <div style={stickySidebar}>
+                <div style={{fontWeight: 'bold'}}>Logged in as</div>
+                <UserComponent user={this.props.user}/>
+                <Button onClick={this.logout}>Log out</Button>
+            </div>
         )
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        user: state.user
+    };
+};
+
+export default connect(mapStateToProps)(SidebarComponent);
