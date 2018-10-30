ad2bd0c0299993b893b541938a16f90b4600c88e
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-30 19:58:46 +0300

    style LoginComponent

--- resources/js/components/LoginComponent.js
+++ resources/js/components/LoginComponent.js
@@ -3,25 +3,24 @@ import {Button} from 'reactstrap';
 import {Redirect} from 'react-router';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
-import center from '../styles/center.css';
 
 //TODO change client_id to real id
 const vk_url = 'https://oauth.vk.com/authorize?client_id=6727304&redirect_uri=http://localhost:8000/vkcallback';
-
-const redirectToVK = () => {
-    window.location.href = vk_url;
-};
 
 class LoginComponent extends Component {
     render() {
         return this.props.isAuthenticated
             ? (
                 <Redirect to='/feed'/>
-
             )
             : (
-                <div style={center.css}>
-                    <Button onClick={redirectToVK}/>
+                <div style={{position: 'fixed', top: '50%', left: '50%'}}>
+                    <a href={vk_url} style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                        <img src='https://www.drupal.org/files/project-images/vkontakte-256.png' height='42' width='42'/>
+                        <div>
+                            Login via VK
+                        </div>
+                    </a>
                 </div>
             );
     }
