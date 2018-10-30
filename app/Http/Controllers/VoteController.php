f25024ddb016f5d9bbb01e7c81332210d06c77cc
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-30 19:29:44 +0300

    add caching in VoteController

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -6,11 +6,40 @@ use Illuminate\Http\Request;
 use App\Vote;
 use App\Option;
 use App\Poll;
+use App\User;
 use Auth;
+use Cache;
 use Illuminate\Support\Facades\DB;
 
 class VoteController extends Controller
 {
+    public const CACHE_TTL = 60 * 6;
+
+    public function show(Request $request, $poll_id)
+    {
+        $data = $request->validate([
+            'vote_id' => 'required|numeric'
+        ]);
+        $isAnonymous = Poll::find($poll_id)->first()->is_anonymous;
+        if ($isAnonymous) {
+            return response()->json(['error' => 'cant get voters of anonymous polls', 403]);
+        }
+
+        $vote_id = $data['vote_id'];
+        $user_cache_ids = Cache::get(Vote::getVotesCacheKey($poll_id, $vote_id));
+        if ($user_cache_ids === null) {
+            $votes = Vote::where([
+                ['poll_id', '=', $poll_id],
+                ['vote_id', '=', $vote_id]
+            ])->get()->toArray();
+            $user_cache_ids = array_map(function($vote) {
+                return User::getCacheKey($vote['user_id']);
+            }, $votes);
+            Cache::put(Vote::getVotesCacheKey($poll_id, $vote_id), $user_cache_ids, self::CACHE_TTL);
+        }
+        return array_values(User::fetchWithCache($user_cache_ids));
+    }
+
     public function store(Request $request)
     {
         $validated_data = $request->validate([
@@ -42,22 +71,28 @@ class VoteController extends Controller
                 $vote->save();
             }, $votes);
         });
+        Cache::forget(Vote::getVotedForCacheKey($user_id, $validated_data['poll_id']));
+        Cache::forget(Option::getCacheKey($validated_data['poll_id']));
+        foreach ($validated_data['option_ids'] as $id) {
+            Cache::tags([$validated_data['poll_id'], $id])->flush();
+        }
         return response()->json(['success' => 'success']);
     }
 
-    public function destroy($poll_id) {
+    public function destroy($poll_id)
+    {
         $user_id = Auth::user()->id;
         $poll = Poll::find($poll_id);
-        if ($poll === null || $poll->user_id !== $user_id) {
+        if ($poll === null) {
             return response()->json(['error' => 'invalid poll id'], 403);
         }
         $votes = Vote::where([
             ['user_id', '=', $user_id],
             ['poll_id', '=', $poll_id]
-        ])->get();
+        ])->get()->toArray();
 
-        DB::transaction(function() use ($votes){
-            array_map(function($vote) {
+        DB::transaction(function () use ($votes) {
+            array_map(function ($vote) {
                 DB::update(
                     'UPDATE options SET vote_count = vote_count - 1 WHERE poll_id = ? AND `index` = ?',
                     [$vote['poll_id'], $vote['vote_id']]
@@ -66,9 +101,14 @@ class VoteController extends Controller
                     'DELETE FROM votes WHERE poll_id = ? AND vote_id = ?',
                     [$vote['poll_id'], $vote['vote_id']]
                 );
-            }, $votes->toArray());
+            }, $votes);
 
         });
+        Cache::forget(Vote::getVotedForCacheKey($user_id, $poll_id));
+        Cache::forget(Option::getCacheKey($poll_id)); // to fetch vote count again
+        foreach ($votes as $vote) {
+            Cache::forget(Vote::getVotesCacheKey($poll_id, $vote['vote_id']));
+        }
         return response()->json(['success' => 'success']);
     }
 }
