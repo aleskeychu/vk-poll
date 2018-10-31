1c6e51f51795ef7c31873221d8b317f81bf77407
Aleksey Churshin <alexchurshin@gmail.com>
2018-10-31 08:43:17 +0300

    add db init scripts

--- start.sh
+++ start.sh
@@ -1,10 +1,21 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 if [[ ! -d "/var/www/vendor" ]];
 then
     cd /var/www
+    php artisan clear-compiled
+    composer install --no-scripts
     composer update
     composer dump-autoload -o
 fi
+
+#if [[ ! -d "/var/www/node_modules" ]];
+#then
+#    cd /var/www
+#    npm install
+#fi
+
+sleep 20
+php artisan migrate
 
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
